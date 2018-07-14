0a4fae8f21aa848e5261918b9cfc140f9c4e3894
Isan-Rivkin <isanrivkin@gmail.com>
2018-07-14 16:50:18 +0300

    initial commit

---
+++ src/main/java/com/perf/isan_yarden/App.java
@@ -0,0 +1,17 @@
+package com.perf.isan_yarden;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+/**
+ * Hello world!m
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
